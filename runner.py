e7eb42ccaef6a29b6197919163e7860336c3338f
Golden Rockefeller <goldenrockefeller@protonmail.com>
2021-11-22 14:55:45 -0500

    Get more stat runs on experiment C1

--- runner.py
+++ runner.py
@@ -90,7 +90,7 @@ class Runner:
         sys.stdout.flush()
 
         args = {
-            "n_steps" : 100,
+            "n_steps" : 1000,
             "n_rows" : 10,
             "n_cols" : 10,
             "horizon" : 1000
@@ -120,7 +120,7 @@ class Runner:
 
         domain = Domain(n_rows, n_cols, n_steps, n_robots, n_req, n_goals)
 
-        n_epochs = 3000
+        n_epochs = 6000
         n_policies = 50
 
         kl_penalty_factor = 10.
