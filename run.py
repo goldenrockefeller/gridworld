5857ac041a76fefd8fe77e5ddb89769693a74088
Golden Rockefeller <goldenrockefeller@protonmail.com>
2021-11-26 01:33:26 -0500

    Add imtc 200 trial

--- run.py
+++ run.py
@@ -8,9 +8,13 @@ from time import sleep
 import sys
 import cProfile, pstats
 
+def imtc_200(args):
+    imtc(args)
+
 def run():
     experiment_name = "MAG_TestD1"
     n_stats_run_per_process = 1
+
 
 
     mods_to_mix = [
