2d90be7d333bb25693d53e09573edaf9ed8ed330
Golden Rockefeller <goldenrockefeller@protonmail.com>
2021-11-30 02:42:29 -0500

    Add uqtc trial with eligibility trace

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def run():
     n_stats_run_per_process = 1
 
     mods_to_mix = [
-        (uqchc_l(1, 0.0), )
+        (uqtc_l(500, 0.05), )
     ]
 
     runners = [
@@ -31,8 +31,6 @@ def run():
 
 
 
-
-# uqtc_l(500, 0.05)
 
 
 
